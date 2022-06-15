0fde4055bacbcc6d0003ce53e130f776429509fd
matsammut <matteo.sammut.20@um.edu.mt>
2022-06-16 00:14:29 +0200

    Getting somewhere w Parser

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <vector>
 #include <iterator>
+#include <string.h>
 using namespace std;
 
 class tableLexerReturn {       // The class
@@ -13,6 +14,30 @@ class tableLexerReturn {       // The class
     string lexemeFin;  // Attribute (string variable)
     int counter;
 };
+
+
+class Statement{
+    public:
+
+};
+
+
+class VD : public Statement{
+    public:
+        bool let;
+        // identifiers are strings
+        string iden;
+        bool colin;
+        // types are 4 different strings
+        string typ;
+        bool equals;
+        //EXPR express;
+};
+
+class EXPR{
+
+};
+
 
 char NextChar(vector<char> input,int decrement){
     static int nextcharpointer = -1;
@@ -152,7 +177,7 @@ tableLexerReturn NextWord(vector<char> input){
     if (std::find(std::begin(Sa), std::end(Sa), state) != std::end(Sa)){
         returnLexer.stackTop = stack.top();
         returnLexer.lexemeFin = lexeme;
-        cout << lexeme <<"\t"<<+ stack.top() << "\n";
+        // cout << lexeme <<"\t"<<+ stack.top() << "\n";
         return returnLexer;
     }
     else{
@@ -161,14 +186,301 @@ tableLexerReturn NextWord(vector<char> input){
     }
 }
 
-int main(){
-    ifstream myfile("example.txt");
-    string line;
+tableLexerReturn getNextToken(vector<char> fileVector, bool lookahead){
+    static size_t notEOF = 0;
     tableLexerReturn ReturnHold;
+
+    if (notEOF != fileVector.size()){
+        if (lookahead){
+            ReturnHold = NextWord(fileVector);
+            for (int i = 0; i < ReturnHold.counter; i++){
+                NextChar(fileVector, -1);
+            }
+            return ReturnHold;
+        }
+        else{
+            ReturnHold = NextWord(fileVector);
+            notEOF += ReturnHold.counter;
+            notEOF -= 1;
+            NextChar(fileVector, -1);
+            return ReturnHold;
+        }  
+    }
+    else{
+        cout << "end";
+        ReturnHold.stackTop = -100;
+        return ReturnHold;
+    }
+
+}
+
+
+
+int functionCall(vector<char> fileVector){
+    vector<string> returnVec;
+    tableLexerReturn nextToken;
+    nextToken = getNextToken(fileVector, false);
+
+    // left Bracket
+    if (nextToken.stackTop == 7){
+
+    }
+    else{
+        cout << "ERROR EXPECTED LEFT BRACKET FOR SUB EXPRESSION";
+        exit(0);
+    }
+
+    // need method to have multiple params
+
+    // right Bracket
+    if (nextToken.stackTop == 12){
+
+    }
+    else{
+        cout << "ERROR EXPECTED RIGHT BRACKET FOR SUB EXPRESSION";
+        exit(0);
+    }
+    return 0;
+
+    return 0;
+}
+
+
+int term(vector<char> fileVector){
+    //make a vector to return the finished statement
+    vector<string> returnVec;
+    // factor(fileVector);
+    return 0;
+}
+
+int simpleExpr(vector<char> fileVector){
+    //make a vector to return the finished statement
+    vector<string> returnVec;
+    term(fileVector);
+    return 0;
+}
+
+
+EXPR expression(vector<char> fileVector){
+    //make a vector to return the finished statement
+    EXPR returnEXPR;
+
+    simpleExpr(fileVector);
+    return returnEXPR;
+}
+
+int actualParams(vector<char> fileVector){
+    expression(fileVector);
+    return 0;
+}
+
+int subExpression(vector<char> fileVector){
+    vector<string> returnVec;
+    tableLexerReturn nextToken;
+    nextToken = getNextToken(fileVector, false);
+
+    // left Bracket
+    if (nextToken.stackTop == 7){
+
+    }
+    else{
+        cout << "ERROR EXPECTED LEFT BRACKET FOR SUB EXPRESSION";
+        exit(0);
+    }
+
+    expression(fileVector);
+
+    // right Bracket
+    if (nextToken.stackTop == 12){
+
+    }
+    else{
+        cout << "ERROR EXPECTED RIGHT BRACKET FOR SUB EXPRESSION";
+        exit(0);
+    }
+    return 0;
+}
+string identifier(vector<char> fileVector){
+    tableLexerReturn nextToken;
+    do{
+        nextToken = getNextToken(fileVector, false);
+    }while(nextToken.stackTop == 33);
+    
+    if (nextToken.stackTop == 6){
+        return nextToken.lexemeFin;
+    }
+    else{
+        cout << "ERROR IDENTIFIER INVALID";
+        exit(0);
+    }
+}
+
+// int factor(vector<char> fileVector){
+//     //make a vector to return the finished statement
+//     vector<string> returnVec;
+//     tableLexerReturn nextToken;
+//     nextToken = getNextToken(fileVector, true);
+    
+//     if (nextToken.stackTop == 7){
+//         subExpression(fileVector);
+//     }
+//     else if(nextToken.lexemeFin == "float" ||nextToken.lexemeFin == "int" ||nextToken.lexemeFin == "char" ||nextToken.lexemeFin == "bool" || nextToken.lexemeFin == "true" ||nextToken.lexemeFin == "false" || nextToken.stackTop == 1 || nextToken.stackTop == 3){
+//         identifier(fileVector);
+//     }
+//     else if(nextToken.stackTop == 6){
+//         identifier(fileVector);
+//         tableLexerReturn nextTokenisFC;
+//         nextTokenisFC = getNextToken(fileVector, true);
+//         if (nextTokenisFC.stackTop == 7){
+//             // function call
+//             functionCall(fileVector);
+//         }
+//     }
+//     else{
+//         cout << "ERROR FACTOR EXPECTED";
+//         exit(0);
+//     }
+//     return 0;
+// }
+
+string type(vector<char> fileVector){
+    tableLexerReturn nextToken;
+    nextToken = getNextToken(fileVector, false);
+    if (nextToken.lexemeFin == "float"){
+        return "float";
+    }
+    else if (nextToken.lexemeFin == "int"){
+        return "int";
+    }
+    else if (nextToken.lexemeFin == "bool"){
+        return "bool";
+    }
+    else if (nextToken.lexemeFin == "char"){
+        return "char";
+    }
+    else{
+        cout << "ERROR TYPE EXPECTED";
+        exit(0);
+    }
+}
+
+Statement* variableDecl(vector<char> fileVector){
+    //make a vector to return the finished statement
+    VD holdVD;
+    VD* returnVD = &holdVD;
+    tableLexerReturn nextToken;
+    do{
+        nextToken = getNextToken(fileVector, false);
+    }while(nextToken.stackTop == 33);
+    
+    if (nextToken.lexemeFin == "let"){
+        returnVD->let = true;
+    }
+    else{
+        returnVD->let = false;
+        cout << "ERROR EXPECTED LET FOR VARIABLE DECLERATION";
+        exit(0);
+    }
+
+    // holds identifier name as a string
+
+    returnVD->iden = identifier(fileVector);
+    
+    do{
+        nextToken = getNextToken(fileVector, false);
+    }while(nextToken.stackTop == 33);
+
+    // 21 being a colin
+    if (nextToken.stackTop == 21){
+        returnVD->colin = true;
+    }
+    else{
+        cout << "ERROR COLIN EXPECTED FOR VARIABLE DECLERATION";
+        exit(0);
+    }
+
+    returnVD->typ = type(fileVector);
+
+    do{
+        nextToken = getNextToken(fileVector, false);
+    }while(nextToken.stackTop == 33);
+    // 13 being an equals
+    if (nextToken.stackTop == 13){
+       returnVD->equals = true;
+    }
+    else{
+        cout << "ERROR COLIN EXPECTED FOR VARIABLE DECLERATION";
+        exit(0);
+    }
+
+    return returnVD;
+    // expression(fileVector);
+}
+
+int parser(vector<char> fileVector){
+    // a program is a Vector of Statements
+    vector<Statement*> *AST = new vector<Statement*>();
+    // vector<Statement*> AST = {};
+    tableLexerReturn nextToken;
+
+    for(;;){
+        nextToken = getNextToken(fileVector, true);
+        // Statement returnStatement;
+        if (nextToken.stackTop == -100){
+            break;
+        }
+        if (nextToken.lexemeFin == "let"){
+
+            AST->push_back(variableDecl(fileVector));
+            do{
+                nextToken = getNextToken(fileVector, false);
+            }while(nextToken.stackTop == 33);
+
+            Statement* printVD = &AST[0];
+            // cout << AST[0]->iden <<"\n";
+            if (nextToken.stackTop == 20){
+                
+            }
+            else{
+                cout << "ERROR EXPECTED SEMICOLIN EXPECTED";
+                exit(0);
+            }
+        }
+        else if (nextToken.lexemeFin == "print"){
+
+        }
+        else if (nextToken.lexemeFin == "return"){
+
+        }
+        else if (nextToken.lexemeFin == "if"){
+            
+        }
+        else if (nextToken.lexemeFin == "for"){
+            
+        }
+        else if (nextToken.lexemeFin == "while"){
+            
+        }
+        else if (nextToken.lexemeFin == "fn"){
+            
+        }
+        else if (nextToken.stackTop == 22){
+            
+        }
+        // add assignment
+               
+        // AST->push_back(returnStatement);
+    }
+    
+    return 0;
+}
+
+int main(){
+    ifstream myfile("under.txt");
+    string line;
     vector<string> tokens; 
     vector<int> stateType;
     vector<char> fileVector; 
-    size_t notEOF = 0;
     
     if (myfile.is_open())
     {
@@ -181,19 +493,13 @@ int main(){
             fileVector.push_back('\n');
         }
         myfile.close();
+
         //print vector as string
         // std::string s(fileVector.begin(), fileVector.end());
         // std::cout << s <<"\n";
-        while (notEOF != fileVector.size()){
-            
-            ReturnHold = NextWord(fileVector);
-            tokens.push_back(ReturnHold.lexemeFin);
-            stateType.push_back(ReturnHold.stackTop);
-            notEOF += ReturnHold.counter;
-            notEOF -= 1;
-            NextChar(fileVector, -1);
-        }
-        // NextChar(fileVector, 0);
+
+        parser(fileVector);
+        
     }
     
 }
@@ -227,3 +533,33 @@ int main(){
 //  return Type[state]; 
 // else return invalid; 
 // }
+
+// if (newState.stateNo == 1){
+//             newState.name = "IntegerLiteral";
+//             newState.value = nextToken.lexemeFin;
+//             AST.push_back(newState);
+//         }
+//         else if (newState.stateNo == 3){
+//             newState.name = "FloatLiteral";
+//             AST.push_back(newState);
+//         }
+//         else if (newState.stateNo == 5){
+//             newState.name = "CharLiteral";
+//             AST.push_back(newState);
+//         }
+//         else if (newState.stateNo == 6){
+//             newState.name = "Identifier";
+//             AST.push_back(newState);
+//         }
+//         else if (newState.stateNo == 17 || newState.stateNo == 19){
+//             newState.name = "MultiplicativeOp";
+//             AST.push_back(newState);
+//         }
+//         else if (newState.stateNo == 18 || newState.stateNo == 15){
+//             newState.name = "AdditiveOp";
+//             AST.push_back(newState);
+//         }
+//         else if (newState.stateNo == 14 || newState.stateNo == 8|| newState.stateNo == 9 || newState.stateNo == 10|| newState.stateNo == 11|| newState.stateNo == 26){
+//             newState.name = "RelationalOp";
+//             AST.push_back(newState);
+//         }
